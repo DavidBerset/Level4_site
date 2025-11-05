e15e37dec8f5a7ea93b7da38e6bf6586ea8cf277
Wix Remote Machine <support@wix.com>
2025-11-05 19:55:41 +0000

    Afficher toutes les réalisations CMS

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -358,7 +358,7 @@ export default function HomePage() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {realisations.slice(0, 6).map((realisation, index) => (
+            {realisations.map((realisation, index) => (
               <motion.div
                 key={realisation._id}
                 initial={{ opacity: 0, y: 30 }}
